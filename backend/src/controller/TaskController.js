873ee70dd56a874c3c5956213343689a1ce64416
Marco Lima <maurelima@gmail.com>
2020-09-02 16:28:03 -0300

    Listando tarefas

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,4 +1,5 @@
 const TaskModel = require("../model/TaskModel");
+const { response } = require("express");
 
 class TaskController {
 
@@ -25,6 +26,17 @@ class TaskController {
         });
 
     }
+
+    async all(req, res){
+        await TaskModel.find( { macaddress: { '$in': req.body.macaddress } } )
+            .sort('when')
+            .then(response => {
+                return res.status(200).json(response);
+            })
+            .catch(error => {
+                return res.status(500).json(error);
+            });
+    }   
 
 }
 
