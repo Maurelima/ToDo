ae71f8ab037716a9285f34f1db15596feaf4be89
Marco Lima <maurelima@gmail.com>
2020-09-03 12:16:10 -0300

    Listando tarefas do dia

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,5 +1,5 @@
 const TaskModel = require("../model/TaskModel");
-const { response } = require("express");
+const { startOfDay, endOfDay } = require('date-fns');
 //const { response } = require("express");
 const current = new Date();
 
@@ -90,6 +90,24 @@ class TaskController {
             return res.status(500).json(error);
         });
     }
+
+    async today(req, res){
+
+        await TaskModel
+        .find({ 
+            'macaddress': {'$in': req.body.macaddress},
+            'when': {'$gte': startOfDay(current), '$lte': endOfDay(current)}
+        })
+        .sort('when')
+        .then(response => {
+            return res.status(200).json(response);
+        })
+        .catch(error => {
+            return res.status(500).json(error);
+        });
+        
+    }
+
 
 }
 
