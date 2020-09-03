6193c5f524a8ba23754bb89584ff9ca8c0b2309f
Marco Lima <maurelima@gmail.com>
2020-09-03 12:25:50 -0300

    Listando tarefas do mês

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,5 +1,5 @@
 const TaskModel = require("../model/TaskModel");
-const { startOfDay, endOfDay, startOfWeek, endOfWeek } = require('date-fns');
+const { startOfDay, endOfDay, startOfWeek, endOfWeek, startOfMonth, endOfMonth } = require('date-fns');
 //const { response } = require("express");
 const current = new Date();
 
@@ -125,6 +125,22 @@ class TaskController {
         
     }
 
+    async month(req, res){
+
+        await TaskModel
+        .find({ 
+            'macaddress': {'$in': req.body.macaddress},
+            'when': {'$gte': startOfWeek(current), '$lte': endOfMonth(current)}
+        })
+        .sort('when')
+        .then(response => {
+            return res.status(200).json(response);
+        })
+        .catch(error => {
+            return res.status(500).json(error);
+        });
+        
+    }
 
 }
 
